ff328e6c62471d4d4855d6d4e115de341a168297
Livier Camilo <kamtorres20@gmail.com>
2018-08-25 12:00:46 -0500

    Implementacion de Base de datos in Memory Storage, insert y select info

---
+++ Net-Technical-Test-V3/src/Net-Technical-Test-V3/Controllers/ClientController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Net_Technical_Test_V3.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Net_Technical_Test_V3.Controllers
+{
+
+   
+    public class ClientController : Controller
+    {
+        private ApplicationDbContext _context;
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public  ClientController(ApplicationDbContext context)
+        {
+
+            _context = context;
+            if (!_context.Clients.Any())
+            {
+                _context.Clients.Add(new Client
+                { Name = "Livier Torres", Email = "Kamtorres20@gmail.com",Nit="123456" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<Client> GetClients()
+        {
+            return _context.Clients;
+        }
+    }
+}
--- Net-Technical-Test-V3/src/Net-Technical-Test-V3/Controllers/HomeController.cs
+++ Net-Technical-Test-V3/src/Net-Technical-Test-V3/Controllers/HomeController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Net_Technical_Test_V3.Models;
 
 namespace Net_Technical_Test_V3.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
         public IActionResult Index()
         {
             return View();
+        }
+
+        public HomeController(ApplicationDbContext context)
+        {
+
+            _context = context;
+            if (!_context.Clients.Any())
+            {
+                _context.Clients.Add(new Client
+                { Name = "Livier Torres", Email = "Kamtorres20@gmail.com", Nit = "123456" });
+                _context.SaveChanges();
+            }
+        }
+
+ 
+
+        public ActionResult GetClients(string id)
+        {
+            IEnumerable<Client>  cl = _context.Clients;
+
+            ModelState.Clear();
+            return PartialView("~/Views/Home/ListClients.cshtml", cl);
         }
 
         public IActionResult About()
