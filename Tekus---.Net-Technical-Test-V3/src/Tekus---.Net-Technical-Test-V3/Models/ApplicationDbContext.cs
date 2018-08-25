054fcca7ca5225f4b0901f7a70750f5382feea09
Livier Camilo <kamtorres20@gmail.com>
2018-08-25 10:41:57 -0500

    Inicio de Proyecto

---
+++ Tekus---.Net-Technical-Test-V3/src/Tekus---.Net-Technical-Test-V3/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tekus___.Net_Technical_Test_V3.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
