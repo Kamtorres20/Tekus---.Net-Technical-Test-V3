ea57b02ad330fdf8c48a0cd814cd3ae709c8acb0
Livier Camilo <kamtorres20@gmail.com>
2018-08-25 11:19:42 -0500

    Inicio de Proyecto

---
+++ Net-Technical-Test-V3/src/Net-Technical-Test-V3/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Net_Technical_Test_V3.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
